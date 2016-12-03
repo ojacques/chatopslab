415db1fda50e5fd222fff6270271ef615d0ac6b6
Olivier Jacques <olivier.jacques@hpe.com>
2016-12-03 17:44:01 +0100

    Add NodeJS install for hubot

--- vagrant/scripts/provision.sh
+++ vagrant/scripts/provision.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+git clone https://github.com/ojacques/chatopslab.git
+
 which curl docker make ruby || {
   sudo apt-get update
   sudo apt-get install -y curl docker.io make ruby
@@ -9,10 +11,24 @@ which curl docker make ruby || {
 
 which docker-compose || {
   curl -sSL https://github.com/docker/compose/releases/download/1.9.0-rc2/docker-compose-`uname -s`-`uname -m` > docker-compose
-  sudo cp docker-compose /usr/local/bin
+  sudo mv docker-compose /usr/local/bin
   sudo chmod +x /usr/local/bin/docker-compose
 }
 
-git clone https://github.com/ojacques/chatopslab.git
-cd chatopslab/app
+which npm || {
+  sudo apt-get install nodejs
+  sudo apt-get install nodejs-legacy
+  sudo apt-get install npm
+}
+
+which yo || {
+  cd ~/chatopslab
+  mkdir hubot && cd hubot
+  sudo npm install -g yo generator-hubot
+}
+
+cd ~chatopslab/app
+echo "Waiting for Docker daemon to start"
+sleep 5
 docker-compose create
+
